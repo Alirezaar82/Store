e7c04b1d372a47c44d1967366a9a9f72a08debeb
Alirezaar82 <alirezaalireza82617@gmail.com>
2024-05-30 20:34:29 +0330

    setup product detail

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
   path('products/grid/',views.ProductsGridView.as_view(),name='products-grid'),
+  path('product/<slug>/detail/',views.ProductDetailView.as_view(),name='product-detail'),
 ] 
--- core/shop/views.py
+++ core/shop/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from django.core.exceptions import FieldError
 
@@ -12,28 +11,35 @@ class ProductsGridView(ListView):
         return self.request.GET.get('page_size', self.paginate_by)
     
     def get_queryset(self):
-        querySet = ProductModel.objects.prefetch_related('category').filter(stock__gte=1,status=ProductStatusType.publish.value)
-        
-        if search_q:=self.request.GET.get('q'):
-            querySet = querySet.filter(title__icontains=search_q)
-        if category:=self.request.GET.get('category_slug'):
-            querySet = querySet.filter(category__slug=category)
-        if min_price:=self.request.GET.get('min_price'):
-            querySet = querySet.filter(price__gte=min_price).order_by('price')
-        if max_price:=self.request.GET.get('max_price'):
-            querySet = querySet.filter(price__lte=max_price).order_by('-price')
-        if order_by:=self.request.GET.get("order_by"):
-            print(order_by)
+        queryset = ProductModel.objects.prefetch_related('category').filter(
+            stock__gte=1,
+            status=ProductStatusType.publish.value
+            )
+        if search_q := self.request.GET.get('q'):
+            queryset = queryset.filter(title__icontains=search_q)
+        if category := self.request.GET.get('category_slug'):
+            queryset = queryset.filter(category__slug=category)
+        if min_price := self.request.GET.get('min_price'):
+            queryset = queryset.filter(price__gte=min_price).order_by('price')
+        if max_price := self.request.GET.get('max_price'):
+            queryset = queryset.filter(price__lte=max_price).order_by('-price')
+        if order_by := self.request.GET.get('order_by'):
             try:
                 queryset = queryset.order_by(order_by)
             except FieldError:
                 pass 
 
-            
-        return querySet
+        return queryset
     
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
         context['categories'] = ProductCategoryModel.objects.all()
         context['total_items'] = self.get_queryset().count()
         return context
+    
+class ProductDetailView(DetailView):
+    template_name = 'shop/product-detail.html'
+    queryset = ProductModel.objects.prefetch_related('category').filter(
+            stock__gte=1,
+            status=ProductStatusType.publish.value
+            )
