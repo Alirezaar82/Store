171ac77f4f147f2de7d53f5d0551e8ff4168d0cf
Alirezaar82 <alirezaalireza82617@gmail.com>
2024-06-08 22:30:10 +0330

    add calculate_total_price for cartmodel

--- core/cart/models.py
+++ core/cart/models.py
@@ -15,6 +15,10 @@ class CartModel(models.Model):
     def __str__(self):
         return f'{self.user}'
     
+    def calculate_total_price(self):
+        return sum(item.product.get_price() * item.quantity for item in self.cart_items.all())
+    
+    
 
 class CartItemModel(models.Model):
     cart = models.ForeignKey(CartModel,on_delete=models.CASCADE)
