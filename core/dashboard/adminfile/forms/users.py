0c1f2743eb27acee1561be78d4aabb7de7d2e98e
Alirezaar82 <alirezaalireza82617@gmail.com>
2024-06-19 22:24:03 +0330

    some fixed code

--- core/dashboard/adminfile/forms/users.py
+++ core/dashboard/adminfile/forms/users.py
@@ -4,12 +4,14 @@ from django import forms
 User = get_user_model()
 
 class UserForm(forms.ModelForm):
-    phone_number = forms.CharField(
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'disabled': 'disabled'
-        })
-    )
+    # phone_number = forms.CharField(
+    #     widget=forms.TextInput(attrs={
+    #         'class': 'form-control mx-3 Disabled text-center mb-3',
+    #         'disabled': 'disabled',
+            
+
+    #     })
+    # )
     class Meta:
         model = User
     
@@ -22,6 +24,12 @@ class UserForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['phone_number'].widget.attrs['class'] = 'form-control mx-3 Disabled text-center mb-3'
+        self.fields['phone_number'].disabled = True
+        self.fields['phone_number'].required = False
+        self.fields['is_active'].required = False
         self.fields['is_active'].widget.attrs['class'] = 'form-check-input mb-3'
+        self.fields['is_verified'].required = False
         self.fields['is_verified'].widget.attrs['class'] = 'form-check-input mb-3'
+        self.fields['type'].required = False
+        self.fields['type'].widget.attrs['class'] = 'form-select'
         
