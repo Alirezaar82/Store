e0d1b669d718abbd1544da15439da0cf1d861288
Alirezaar82 <alirezaalireza82617@gmail.com>
2024-06-19 23:53:29 +0330

    fix some bugs

--- core/dashboard/adminfile/forms/review.py
+++ core/dashboard/adminfile/forms/review.py
@@ -1,14 +1,20 @@
 from django import forms
-from django.core.exceptions import ValidationError
-
 from shop.models import ReviewModel
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = ReviewModel
-        fields=[            
-            'status',
-        ]
+        fields = [
+            "description",
+            "rate",
+            "status",
+        ] 
     def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['status'].widget.attrs['class'] = 'form-select'
+        super().__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs['class'] = 'form-control'
+        self.fields['description'].disabled = True
+        self.fields['description'].required = False
+        self.fields['rate'].widget.attrs['class'] = 'form-control '
+        self.fields['rate'].required = False
+        self.fields['rate'].disabled = True
+        self.fields['status'].widget.attrs['class'] = 'form-select'
